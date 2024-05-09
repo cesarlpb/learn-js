8d63dcb2d57a3bf628402f3d952363aa08359b24
César <cesarlpb@gmail.com>
2024-05-09 11:37:58 +0200

    Ej 03 código final

---
+++ ejercicios/02.variables/03.js
@@ -0,0 +1,18 @@
+/*
+
+Un poco de interactividad. En este ejercicio debes escribir 
+un programa que lea el nombre del usuario y salude con un 
+"Hola" seguido del nombre del usuario.
+
+Para leer el nombre usa un prompt() con un texto indicativo 
+como "Escribe tu nombre"
+
+input: nombre -> "Pepe"
+output: mensaje -> "Hola, Pepe"
+
+*/
+
+// Pedimos el nombre del usuario:
+let nombre = prompt("¿Cómo te llamas?");
+let saludo = "Hola, " + nombre;
+alert(saludo); 
