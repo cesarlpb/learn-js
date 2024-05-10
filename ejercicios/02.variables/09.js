95351adea494d83c5f2e627ec201b1439aba241c
César <cesarlpb@gmail.com>
2024-05-10 10:40:28 +0200

    Ejercicio 09 enunciado

---
+++ ejercicios/02.variables/09.js
@@ -0,0 +1,25 @@
+/* 
+
+Este ejercicio es todo un clásico si lo haces a la primera 
+y sin tardar mucho, puedes estar seguro: 
+entiendes las variables.
+
+Teiens que escribir un programa que intercambie el valor 
+de dos variables. 
+
+Al final la primera variable debe tener 
+el valor de la segunda y la segunda el valor de la primera. 
+
+No vale usar arrays ni asignación múltiple.
+
+inputs: var1, var2
+
+var1 = 1;
+var2 = 2;
+
+output: var1, var2
+
+var1 -> 2
+var2 -> 1
+
+*/
