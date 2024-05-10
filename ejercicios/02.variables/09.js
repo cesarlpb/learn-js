2b4899dbadb6dae110afa79c69ac7fac85567a69
César <cesarlpb@gmail.com>
2024-05-10 12:46:31 +0200

    Ejercicio 09 código final

--- ejercicios/02.variables/09.js
+++ ejercicios/02.variables/09.js
@@ -23,3 +23,22 @@ var1 -> 2
 var2 -> 1
 
 */
+
+console.log("--- Ejercicio 09 ---");
+
+let var1 = prompt("Introduce el primer valor:");
+let var2 = prompt("Introduce el segundo valor:");
+
+console.log("Valores recibidos:")
+console.log("var1, var2:", var1, var2);
+
+let temp = var2;
+var2 = var1;
+
+// Usamos temp para guardar temporalmente el valor 
+// de una de las dos variables:
+
+var1 = temp; // valor anterior de var2
+
+console.log("Valores intercambiados:")
+console.log("var1, var2:", var1, var2);
