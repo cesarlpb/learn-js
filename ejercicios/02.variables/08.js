173eecb0783476c3d25b91ef43f4f76e979148c6
César <cesarlpb@gmail.com>
2024-05-10 10:38:22 +0200

    Ejercicio 08 enunciado

---
+++ ejercicios/02.variables/08.js
@@ -0,0 +1,20 @@
+/*
+Una tortilla de patatas lleva 200 gramos de patatas por persona.
+Por cada kilo de patatas se necesitan 5 huevos y 
+300 gramos de cebolla. 
+
+Escribe un script que dado el número de comensales calcule 
+las cantidades de ingredientes necesarias
+
+input: comensales (número de personas)
+output: ingrediente -> gramosPatatas, huevos, gramosCebolla
+
+Ejemplo:
+
+1 comensal
+
+g Patatas => 200
+huevos    => 200 / 1000 = 0.2 => 0.2 * 5 = 1
+g cebolla => 200 / 1000 = 0.2 => 0.2 * 300 = 60
+
+*/
