ef2cc48ee39f8cf6f7bbd5ebe705769656f69c92
César <cesarlpb@gmail.com>
2024-05-10 10:42:34 +0200

    Ejercicio 10 enunciado

---
+++ ejercicios/02.variables/10.js
@@ -0,0 +1,12 @@
+/*
+Pues seguimos con los números. 
+
+Escribe un script que dado un número nos devuelva true 
+si es un número par y false si es un número impar. 
+
+No usar condicionales.
+
+input: num -> entero o int
+
+output: boolean -> true o false
+*/
