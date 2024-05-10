8ef167407b878b486747531d7ae4eea75b90cae5
César <cesarlpb@gmail.com>
2024-05-10 12:41:35 +0200

    Ejercicio 08 código final

--- ejercicios/02.variables/08.js
+++ ejercicios/02.variables/08.js
@@ -18,3 +18,35 @@ huevos    => 200 / 1000 = 0.2 => 0.2 * 5 = 1
 g cebolla => 200 / 1000 = 0.2 => 0.2 * 300 = 60
 
 */
+
+console.log("--- Ejercicio 08 ---");
+
+const GRAMOS_PATATAS_PERSONA = 200;
+const HUEVOS_KG_PATATAS = 5;
+const GRAMOS_CEBOLLA_KG_PATATA = 300;
+
+let comensales = Number.parseInt(prompt("Introduce el número de comensales:"));
+console.log("Comensales:", comensales);
+
+let gramosPatatas = GRAMOS_PATATAS_PERSONA * comensales; // gramos de patatas
+let kgPatatas = gramosPatatas / 1000; // es un número que si puede tener decimales
+
+console.log("gramos y kg de patatas:", gramosPatatas, kgPatatas)
+
+let huevos = kgPatatas * HUEVOS_KG_PATATAS;
+let gramosCebolla = kgPatatas * GRAMOS_CEBOLLA_KG_PATATA;
+
+console.log(gramosPatatas, huevos, gramosCebolla)
+
+// Definimos los strings (cadenas de texto) con variables que sustituye JS
+// que vamos a colocar en el documento HTML:
+let input = `Para ${comensales} comensales se necesitan:`;
+let output = `<ul>
+                <li>${gramosPatatas} g de patatas</li>
+                <li>${huevos} huevos</li>
+                <li>${gramosCebolla} g de cebolla</li>
+              </ul>`;
+
+// Introducimos los valores de esos strings en los elementos del documento HTML:
+document.getElementById("ej-8-input").innerText = input;
+document.getElementById("ej-8-output").innerHTML = output;
