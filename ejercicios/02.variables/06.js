8adc15a51a42246ab3dc900b3d4a372c67224d5c
César <cesarlpb@gmail.com>
2024-05-10 10:32:03 +0200

    Ejercicio 06 enunciado

---
+++ ejercicios/02.variables/06.js
@@ -0,0 +1,17 @@
+/*
+Cambio de unidades. 
+En este ejercicio debes convertir a segundos 
+una medida de tiempo dada en horas y minutos.
+
+Recuerda una hora son 60 minutos y cada minuto son 60 segundos.
+
+inputs: horas, minutos
+output: segundos
+
+Ejemplo:
+
+hh mm -> 1 45
+1 * 3600 + 45 * 60
+segundos
+
+*/
