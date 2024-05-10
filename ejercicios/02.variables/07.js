2e17c84fc0be467c74dc159450dc2c50fe61cbd3
César <cesarlpb@gmail.com>
2024-05-10 10:34:55 +0200

    Ejercicio 07 enunciado

---
+++ ejercicios/02.variables/07.js
@@ -0,0 +1,26 @@
+/*
+En este script debes pedir al usuario un número de dos dígitos 
+y debes devolver el número de unidades y de decenas, o sea, 
+cada dígito del número. 
+
+Usa solo operationes aritméticas
+
+Recuerda si divides un número entre 10 el cociente entero 
+es el número de decenas y el resto es el número de unidades
+
+input: número de dos dígitos
+outputs: decenas, unidades
+
+Ejemplo:
+
+input: 78 
+
+decenas -> 78 / 10 -> parte entera -> parseInt(7.8) => 7
+unidades -> 78 - decenas * 10 = 8
+
+outputs:
+
+decenas = 7
+unidades = 8
+
+*/
