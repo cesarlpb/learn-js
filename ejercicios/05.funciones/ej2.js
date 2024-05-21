47259b7f4e21768ad3f2652230c85af32944c5c1
César <cesarlpb@gmail.com>
2024-05-21 19:33:35 +0200

    Ejercicio 02 código inicial

---
+++ ejercicios/05.funciones/ej2.js
@@ -0,0 +1,8 @@
+/**
+ * Esta función se va a llamar esPar(num) y deberá devolver verdadero si el argumento es par y falso si es impar. Se acepta que solo se usarán números válidos.
+ * 
+ * esPar(2) // true
+ * esPar(3) // false
+ * 
+ */
+
