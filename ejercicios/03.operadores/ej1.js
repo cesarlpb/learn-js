15e681ad05641a2b463692ee83d00a6b3fc70b5d
César <cesarlpb@gmail.com>
2024-05-21 17:41:34 +0200

    Ej 01 enunciado

---
+++ ejercicios/03.operadores/ej1.js
@@ -0,0 +1,6 @@
+/*
+Crea una función que tenga dos argumentos: num1, num2, y que devuelva como resultado la suma de ambos números. Se supone que se usarán solo números válidos, no letras o símbolos.
+
+Llamo a la función como sumar(3,5) deberá devolver 8.
+*/
+
