241f613c58c1adc161c1d9232122627f03a72066
César <cesarlpb@gmail.com>
2024-05-21 13:02:54 +0200

    String

--- cliente/04.tipos_de_datos.js
+++ cliente/04.tipos_de_datos.js
@@ -11,4 +11,23 @@
  * 8. Object
  */
 
+// String -> texto
+// Se definen por '' o "" o `` 
+  // En el caso de `` podemos colocar variables con ${} -> `${2+2}` o `Hola, ${miVar}`
+  // También, `` permite saltar línea
+
+let miString = "Hola, mundo";
+let str = 'Texto 1';
+// Se introduce un salto de línea antes del string:
+let str2 = `
+  Texto 2 
+  con suma: 
+  ${2+2}`;
+// Las variables String se pueden concatenar con +
+let textoFinal = miString + "\n" + str + "\n" + str2;
+console.log(textoFinal);
+// Si se hacen operaciones numéricas, se transforma a string el resultado:
+console.log(2 + '2'); // el resultado es String
+// typeof me permite comprobar si una variable es de tipo String -> 'string'
+console.log(typeof("Texto") == 'string'); // true
 
