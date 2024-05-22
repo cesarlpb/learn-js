19b31c3c7cf08150675cbc6d51d97cdc8b0b87d8
César <cesarlpb@gmail.com>
2024-05-22 19:40:08 +0200

    Añado bucle while
    - Pedimos números si divisor es cero

--- ejercicios/06.bucles/1.if/ej3.js
+++ ejercicios/06.bucles/1.if/ej3.js
@@ -18,12 +18,23 @@ function dividir(a, b) {
   }
   return a / b;
 }
+// let a = parseInt(prompt("Ingresa el dividendo:"));
+// let b = parseInt(prompt("Ingresa el divisor:"));
 
-let a = parseInt(prompt("Ingresa el dividendo:"));
-let b = parseInt(prompt("Ingresa el divisor:"));
+// Realizamos la operación:
+// dividir(a, b);
 
-dividir(a, b);
-
+// ¿Cómo vuelvo a pedir datos si tengo b = 0?
+let a, b;
+do {
+  a = parseInt(prompt("Ingresa el dividendo:"));
+  // TODO 2: pedir solo b si es cero:
+  b = parseInt(prompt("Ingresa el divisor:"));
+  // TODO: colocar un if para evitar llamada si b es 0
+  let cociente = dividir(a, b);
+  console.log("b es: ", b);
+  console.log("cociente:", cociente);
+} while(b == 0);
 
 // print
 
