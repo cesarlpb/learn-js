4fd859422f0f88fae657f71b1722812dee370ce5
César P. <cesarlpb@gmail.com>
2024-10-18 11:49:45 +0200

    solución 03

--- cliente/02.variables/03.js
+++ cliente/02.variables/03.js
@@ -4,3 +4,10 @@ que lea el nombre del usuario y salude con un "Hola" seguido del nombre del usua
 
 Para leer el nombre usa un prompt con un texto indicativo como "Escribe tu nombre"
 */
+
+// Pedimos el nombre del usuario:
+let nombre = prompt("Introduce tu nombre:");
+// Creamos el mensaje para saludar:
+let mensaje = "Hola, " + nombre;
+// Usamos alert para presentar el mensaje:
+alert(mensaje);
