e2d14d3e9c5ec90b4d58754aa82fafb64aa47553
César P. <cesarlpb@gmail.com>
2024-10-18 12:31:41 +0200

    solución 04

--- cliente/02.variables/04.js
+++ cliente/02.variables/04.js
@@ -4,3 +4,11 @@ y el script mostrará como resultado el valor medio de los tres.
 
 Recuerda que la media de tres números se calcula sumando los tres y dividiendo entre 3.
 */
+// Pedimos los tres números:
+let num1 = Number(prompt("Introduce primer número:"));
+let num2 = Number(prompt("Introduce segundo número:"));
+let num3 = Number(prompt("Introduce tercer número:"));
+// Calculamos la media:
+let media = (num1 + num2 + num3) / 3;
+// Añadimos texto (append) con el valor de la variable media:
+document.getElementById("media").innerText += media;
