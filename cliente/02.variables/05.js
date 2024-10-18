c0a40a406324d8f80a05d635757b64823362dc0d
César P. <cesarlpb@gmail.com>
2024-10-18 12:48:24 +0200

    solución 05

--- cliente/02.variables/05.js
+++ cliente/02.variables/05.js
@@ -1,5 +1,15 @@
 /*
-Escribe un programa que solicite al usuario ingresar el número de kilómetros recorridos por su coche y el número de litros consumidos . El script debe mostrar el consumo de combustible por kilómetro.
+Escribe un programa que solicite al usuario ingresar el número de kilómetros recorridos por su coche y el número de litros consumidos. 
+
+El script debe mostrar el consumo de combustible por kilómetro.
 
 Un problema matemático muy simple numero de litros dividido por número de kilómetros.
 */
+
+// Pedimos los datos iniciales:
+let km = Number(prompt("Introduce km:"));
+let litros = Number(prompt("Introduce litros consumidos:"));
+// Realizamos el cálculo del consumo:
+let consumo = litros / km;
+// Escribimos el resultado en el elemento consumo:
+document.getElementById("consumo").innerText += consumo + " L / km";
