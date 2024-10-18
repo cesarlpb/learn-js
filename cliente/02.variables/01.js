d838f2c1a903839d01b0d866a4f0f93f258f58c9
César P. <cesarlpb@gmail.com>
2024-10-18 10:09:31 +0200

    solución 01

--- cliente/02.variables/01.js
+++ cliente/02.variables/01.js
@@ -5,3 +5,13 @@ Haz un script que te calcule el precio total que vas a pagar por tu compra.
 Te recuerdo que para calcular el total debes sumar al precio el resultado 
 de multiplicasr precio por el iva y dividir por 100.
 */
+
+// Iniciamos las variables necesarias para calcular el resultado:
+let precio = 200;
+let iva = 21; // %
+// Cálculo de los impuestos a pagar:
+let impuestos = precio * iva / 100;
+// Calculamos el precio final del producto:
+let precioTotal = precio + impuestos;
+// Escribimos el resultado en consola:
+console.log("El precio final es: " + precioTotal);
