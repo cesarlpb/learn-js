d3db4d35618370b70e35c876bc246c97ca593811
César P. <cesarlpb@gmail.com>
2024-10-21 12:29:03 +0200

    solución 02 if ternario switch

--- cliente/04.if-ternario-switch/02.js
+++ cliente/04.if-ternario-switch/02.js
@@ -1,3 +1,23 @@
-// El usuario debe entrar un número y su cuadrado. Si es correcto el script enviará un mensja ede acierto en caso contrario dirá que se produjo un error.
+// El usuario debe entrar un número y su cuadrado. Si es correcto el script enviará un mensaje de acierto, en caso contrario, dirá que se produjo un error.
+
+// ejemplo: 
+// a = 3 y b = 9
 
 // Se trata de usar el if resumido (cond?true:false)
+
+// inputs
+let numero = Number(prompt("Introduce un número:"));
+let cuadrado = Number(prompt("Introduce el cuadrado del número anterior:"));
+// main
+
+    // if-else
+    let cuadradoCorrecto = numero ** 2;
+    if (cuadrado === cuadradoCorrecto){
+        alert("Es correcto!")
+    } else {
+        alert("No es correcto :(")
+    }
+
+    // ternario
+
+// output
