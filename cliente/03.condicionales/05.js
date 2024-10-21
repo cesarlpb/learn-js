b4c216368bdc765cb11af78000f64b0efdd82f67
César P. <cesarlpb@gmail.com>
2024-10-21 10:49:27 +0200

    modificación ej 05 de If

--- cliente/03.condicionales/05.js
+++ cliente/03.condicionales/05.js
@@ -6,10 +6,16 @@ let a = Number(prompt("Introduce el primer número:"));
 let b = Number(prompt("Introduce el segundo número:"));
 
 // Incluimos el caso de igual en la primera condición (simplificación)
-if (a >= b){
+
+// Modificación: añadimos condición para el igual
+if(a == b){
+    alert("Los números son iguales")
+} else if (a > b){
     let diferencia = a - b;
     alert("Diferencia: " + diferencia);
 } else {
     let diferencia = b - a;
     alert("Diferencia: " + diferencia);
 }
+// TODO: validar que no nos pasan datos no numéricos
+// En caso de que no sean números presentamos mensaje de error o no se ejecuta el programa
