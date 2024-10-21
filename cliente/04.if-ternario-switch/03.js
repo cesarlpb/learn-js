7f9e23f18c9d1ac40c6c3e3b09c6633cfb5a4692
César P. <cesarlpb@gmail.com>
2024-10-21 13:15:03 +0200

    solución 03 if ternario switch

--- cliente/04.if-ternario-switch/03.js
+++ cliente/04.if-ternario-switch/03.js
@@ -1,1 +1,16 @@
-// En este caso, también usando el operador ternario o if reducido, un visitante a la página deberá teclear su edad, si es igual o mayor de 18 recibirá el mensaje de "Entra" en caso contario deberá decirle los años que tiene que estperar para entrar
+// En este caso, también usando el operador ternario o if reducido, un visitante a la página deberá teclear su edad, si es igual o mayor de 18 recibirá el mensaje de "Entra" en caso contario deberá decirle los años que tiene que esperar para entrar
+
+// inputs
+let edad = parseInt(prompt("Introduce tu edad:")); // parseInt redondea hacia abajo
+let mensaje = "Mensaje por defecto"; // iniciamos con un valor 
+
+// ternario
+
+    // ? :
+    edad >= 18 
+        ? mensaje = "Puedes pasar" 
+        : mensaje = `Debes esperar ${18 - edad} año(s)` // string interpolado
+
+// outputs
+
+alert(mensaje);
