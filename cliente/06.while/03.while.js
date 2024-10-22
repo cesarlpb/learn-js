1e2fbf30a63ab22c6424c401c9e4f2ce0f1e0597
César P. <cesarlpb@gmail.com>
2024-10-22 13:10:16 +0200

    solución 03 de while

--- cliente/06.while/03.while.js
+++ cliente/06.while/03.while.js
@@ -1,1 +1,17 @@
-// Un script que preguntará al usuario si desea continuar con el programa. Si el usuario pulsa una letra n (mayúscla o minúscula), el programa finalizará. Cualquier otra entrada repetirá la pregunta
+// Un script que preguntará al usuario si desea continuar con el programa. Si el usuario pulsa una letra n (mayúscula o minúscula), el programa finalizará. Cualquier otra entrada repetirá la pregunta
+
+// inputs
+let letra = ""; // variable para guardar la letra que introduce el usuario
+letra = prompt("¿Quieres continuar el programa (S | N)?")
+
+// bucle while
+
+    // posibles mejoras: usar un array para los valores a comprobar
+    // const valoresAceptados = ["n", "N"];
+    // condicion: valoresAceptados.includes(letra)
+
+while(letra != "n" && letra != "N"){
+    letra = prompt("¿Quieres continuar el programa (S | N)?")
+}
+// outputs
+alert("El programa ha finalizado");
