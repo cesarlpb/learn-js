9ba0a76e3660194e96761c7505135c4a45923cf6
César P. <cesarlpb@gmail.com>
2024-10-22 12:32:57 +0200

    solución 01 while

--- cliente/06.while/01.while.js
+++ cliente/06.while/01.while.js
@@ -1,1 +1,18 @@
 // Usando un bucle while escribir un script que pida un valor entero y cree una lista con los números desde el 0 al valor tecleado. Luego deberá sacar esa lista con un alert. Los números se separarán por comas.  
+
+// inputs
+    // declaramos las variables que necesitamos
+
+let int = 0, list = ""; // entero y string para el mensaje de salida
+int = parseInt(prompt("Introduce un número entero:"));
+
+// bucle while
+let contador = 0; // casi siempre (99.99%) es int
+while(contador <= int){
+    list += `${contador}, `;
+    contador = contador + 1;
+}
+
+// outputs
+    // cortamos los últimos dos caracteres para no acabar la lista con ", "
+alert(list.slice(0, list.length - 2))
