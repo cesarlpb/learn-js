c1502d7e4492f35f8a4994e0f87fb736cc066b3b
César P. <cesarlpb@gmail.com>
2024-10-22 10:01:56 +0200

    enunciados 01 al 03

---
+++ cliente/05.for/01.for.js
@@ -0,0 +1,1 @@
+// Usando un bucle for escribir un script que pida un valor entero y muestre en pantalla una lista de números desde el 0 al valor tecleado. Los números se separarán por comas.  
---
+++ cliente/05.for/02.for.js
@@ -0,0 +1,1 @@
+// Se trata de que dado un número entero el script muestre una cuenta atrás, es decir, una lista de numeros separados por comas desde el número tecleado hasta el 0.
