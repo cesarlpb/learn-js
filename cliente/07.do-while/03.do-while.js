0e4951b56b5338222f978d4c392b55b59bf2a7cc
César P. <cesarlpb@gmail.com>
2024-10-23 10:21:44 +0200

    solución 03 do while

--- cliente/07.do-while/03.do-while.js
+++ cliente/07.do-while/03.do-while.js
@@ -1,1 +1,9 @@
 // En este script vas a crear un menu con tres opciones: escribir, leer, salir. El usuario elegirá una opción y en un alert se le indicará la opción elegida. El programa vuelve a mostrar el menu y se repite el proceso. El script acabará cuando se pulse la opción de salir.
+
+let opcion = "";
+
+do {
+    opcion = prompt("Elige una opción:\n-escribir\n-leer\n-salir");
+} while(opcion != "salir")
+
+alert("Fin del programa");
