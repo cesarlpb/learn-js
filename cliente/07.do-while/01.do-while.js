0a041def735c7b4fdd415f2ecb372fce2a6c1230
César P. <cesarlpb@gmail.com>
2024-10-23 10:18:25 +0200

    solución 01 do while

--- cliente/07.do-while/01.do-while.js
+++ cliente/07.do-while/01.do-while.js
@@ -1,1 +1,8 @@
-// Realiza un script que le pida al usuario que teclee la letra A (en mayúscula) y mientras no lo haga se lo vuelva a pedir. Usa un bulce do..while y la ventana alert para los mensajes
+// Realiza un script que le pida al usuario que teclee la letra A (en mayúscula) y mientras no lo haga se lo vuelva a pedir. Usa un bucle do..while y la ventana alert para los mensajes
+
+let letra = "";
+do {
+    letra = prompt("Dame una letra:");
+} while(letra != "A");
+
+alert("Fin del programa");
