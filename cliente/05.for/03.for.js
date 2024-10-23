a530fe6dff7846b9ebdf5eb0d276282e9fba2d3a
César P. <cesarlpb@gmail.com>
2024-10-23 10:14:48 +0200

    solución 03 for

--- cliente/05.for/03.for.js
+++ cliente/05.for/03.for.js
@@ -1,2 +1,17 @@
 // Se quiere un bucle que genere una lista con todos los números pares positivos por debajo del número tecleado por el usuario. Los números se aparecerán en una ventana alert con cambios de linea (carácter '\n')
 
+// inputs
+// declaramos las variables que necesitamos
+
+let int = 0, list = ""; // entero y string para el mensaje de salida
+int = parseInt(prompt("Introduce un número entero:"));
+
+// bucle for
+for(let contador = 0; contador <= int; contador+=2){
+    list += `${contador}\n`;
+}
+
+// variante: se puede colocar un if para evitar añadir los caractes ", " en la última iteración así nos ahorraríamos el slice o podrías ahorrarte el if haciendo append después del bucle
+
+// outputs
+alert(list);
