c01abc4db06b50f466253433eccaa51d86bb2fd6
César <cesarlpb@gmail.com>
2025-02-10 13:25:34 +0100

    actualizo enunciados 04 y 05

---
+++ cliente/ejercicios/02.variables/enunciados/04.js
@@ -0,0 +1,24 @@
+/*
+En este ejercicio vas a pedir al usuario que teclee 
+tres números enteros 
+y el script mostrará como resultado el valor medio de los tres.
+
+Recuerda que la media de tres números se calcula sumando los tres 
+y dividiendo entre 3.
+
+inputs: num1, num2, num3 // enteros => no lo validamos -> number
+output: promedio         // floats  => number
+
+Ejemplo:
+inputs: 10, 20, 30
+output: 60 / 3 = 20
+
+*/
+
+// Pasos para resolver
+
+// 1. Declaramos variables necesarias (inputs)
+
+// 2. Realizamos operaciones necesarias
+
+// 3. Escribimos el resultado (output)
---
+++ cliente/ejercicios/02.variables/enunciados/05.js
@@ -0,0 +1,17 @@
+/*
+Escribe un programa que solicite al usuario ingresar el número de kilómetros recorridos por su coche y el número de litros consumidos. 
+El script debe mostrar el consumo de combustible por kilómetro.
+
+Un problema matemático muy simple número de litros dividido por número de kilómetros.
+
+input: km, litros
+output: consumo en L / km
+*/
+
+// Pasos para resolver
+
+// 1. Declaramos variables necesarias (inputs)
+
+// 2. Realizamos operaciones necesarias
+
+// 3. Escribimos el resultado (output)
