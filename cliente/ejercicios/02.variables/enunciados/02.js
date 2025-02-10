d9e0b3d5ee9e237ec5254d9284c851bfd4c9b631
César <cesarlpb@gmail.com>
2025-02-10 13:06:08 +0100

    añado enunciado y solución 02

---
+++ cliente/ejercicios/02.variables/enunciados/02.js
@@ -0,0 +1,30 @@
+/*
+
+En una variable tienes el lado de un cuadrado, 
+debes escribir un programa que te calcule el 
+área y el perímetro del cuadrado.
+
+lado      -> área = lado * lado
+perímetro -> P = lado + lado + lado +lado = 4 * lado
+
+- El área la calculas como lado multiplicado por lado. 
+- El perímetro es la suma de los cuatro lados.
+
+inputs  -> numbers
+outputs -> numbers
+
+Ej: 
+
+Lado: 10
+Área: 10*10 = 100
+Perímetro: 4*10 = 40
+
+*/
+
+// Pasos para resolver
+
+// 1. Declaramos variables necesarias (inputs)
+
+// 2. Realizamos operaciones necesarias
+
+// 3. Escribimos el resultado (output)
