cad801ba29ee30bdde68b9ea10efb688eeb0822a
César <cesarlpb@gmail.com>
2025-02-10 12:44:13 +0100

    actualizo enunciado 01 de variables

---
+++ cliente/ejercicios/02.variables/enunciados/01.js
@@ -0,0 +1,20 @@
+/*
+
+Has hecho una compra y sabes el precio del producto y su IVA. 
+Haz un script que te calcule el precio total que vas a pagar por tu compra. 
+
+Te recuerdo que para calcular el total debes sumar al precio el resultado 
+de multiplicar precio por el IVA y dividir por 100.
+
+inputs: precio, IVA   -> numbers
+output: precio total  -> number
+
+Ejemplo:
+
+inputs: 100, 21 -> €, %
+output: 121     -> €
+
+inputs: 200, 10 -> €, %
+output: 220     -> €
+
+*/
