19c4a32163dcd80cc98900e6156266432126dfd0
César <cesarlpb@gmail.com>
2025-02-11 09:54:53 +0100

    añado comentarios en ej 03 sobre prompt y alert

--- cliente/ejercicios/02.variables/soluciones/solucion_03.js
+++ cliente/ejercicios/02.variables/soluciones/solucion_03.js
@@ -15,8 +15,19 @@ output: mensaje -> "Hola, Pepe" -> string
 // Pasos para resolver
 
 // 1. Declaramos variables necesarias (inputs)
+
+/* 
+ * prompt() es una función que viene disponible en Javascript en el navegador
+ * Sirve para recibir datos del usuario en formato texto -> es posible que debamos 
+ * convertir el input a otro formato (ej: number) en algunos casos 
+*/
 let nombre = prompt("¿Cómo te llamas?"); // usamos prompt del navegador
 // 2. Realizamos operaciones necesarias
 let saludo = "Hola, " + nombre;
 // 3. Escribimos el resultado (output)
-alert(saludo) // imprime el saludo en una caja en el navegador
+
+/*
+ * alert() es otra función en Javascript disponible en el navegador
+ * Muestra una caja con el mensaje que le pasemos y hay un botón para cerrar la caja 
+ */
+alert(saludo); // -> imprime el saludo en una caja en el navegador
