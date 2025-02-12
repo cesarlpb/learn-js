a799d7fbec14f5fd5d9936f37acce464bec5d7df
César <cesarlpb@gmail.com>
2025-02-12 13:11:55 +0100

    explicación en comentario

---
+++ cliente/ejercicios/02.variables/soluciones/solucion_09.js
@@ -0,0 +1,36 @@
+/*
+Este ejercicio es todo un clásico si lo haces a la primera y sin tardar mucho, 
+puedes estar seguro: entiendes las variables.
+
+Tienes que escribir un programa que intercambie el valor de dos variables. 
+a = 1
+b = 2
+
+Al final la primera variable debe terner el valor de la segunda 
+y la segunda el valor de la primera. No vale usar arrays ni asignación múltiple
+
+...
+b // 1
+a // 2
+
+inputs:  var1, var2
+outputs: var1, var2
+
+Ejemplo:
+var1 = 1
+var2 = 2
+...
+var1 -> 2
+var2 -> 1
+
+*/
+
+/******************************************************************************/
+
+// Pasos para resolver
+
+// 1. Declaramos variables necesarias (inputs)
+
+// 2. Realizamos operaciones necesarias
+
+// 3. Escribimos el resultado (output)
