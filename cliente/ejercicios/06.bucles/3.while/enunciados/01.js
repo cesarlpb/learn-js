62a85a30775a8a2d1291f6e4cd153aa3f65e3b6b
César <cesarlpb@gmail.com>
2025-02-17 13:09:23 +0100

    enunciado 01 de bucle while

---
+++ cliente/ejercicios/06.bucles/3.while/enunciados/01.js
@@ -0,0 +1,19 @@
+// Ej 01
+
+// Usando un bucle while escribir un script que pida un valor entero y cree una 
+// lista con los números desde el 0 al valor tecleado. Luego deberá sacar esa 
+// lista con un alert o console.log(). 
+// 
+// Los números se separarán por comas.  
+
+// Si le doy el número 5 pues deberá contar 0,1,2,3,4,5
+
+/******************************************************************************/
+
+// Pasos para resolver
+
+// 1. Declaramos variables necesarias (inputs)
+
+// 2. Realizamos operaciones y bucles necesarios
+
+// 3. Escribimos el resultado (output)
