02c04a09fffb7115b30c57a24e60fc410dd2fd92
César <cesarlpb@gmail.com>
2025-02-19 12:50:11 +0100

    función restar

---
+++ cliente/ejercicios/07.funciones/1.restar.js
@@ -0,0 +1,15 @@
+// 1. Planteamiento
+
+// 2. Lógica
+
+// 3. Sintaxis 
+
+/**
+ * Calcula la resta de dos números
+ * @param {number} a 
+ * @param {number} b 
+ * @returns devuelve a menos b
+ */
+function restar(a, b){
+  return a - b;
+}
