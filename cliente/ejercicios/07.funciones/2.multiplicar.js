526ca72a0a957fe44992f9e58e8e04f64b670225
César <cesarlpb@gmail.com>
2025-02-19 12:56:57 +0100

    función multiplicar

---
+++ cliente/ejercicios/07.funciones/2.multiplicar.js
@@ -0,0 +1,15 @@
+// 1. Planteamiento
+
+// 2. Lógica
+
+// 3. Sintaxis 
+
+/**
+ * Calcula el producto de dos números
+ * @param {number} a 
+ * @param {number} b 
+ * @returns producto
+ */
+function multiplicar(a, b){
+  return a * b;
+}
