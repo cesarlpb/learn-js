c5af7d696678845102eeb46c329316bb4c124217
César <cesarlpb@gmail.com>
2025-02-19 12:48:21 +0100

    función sumar

---
+++ cliente/ejercicios/07.funciones/0.sumar.js
@@ -0,0 +1,37 @@
+// 1. Planteamiento
+
+// inputs: a, b  (numbers)
+// output: a + b (number)
+
+// La función retorna la suma -> return
+
+// 2. Lógica
+
+// a y b -> a + b
+// A partir de dos números calculamos la suma
+
+// 3. Sintáxis 
+
+/**
+ * Pseudocódigo: 
+ * 
+ * function sumar (number a, number b){
+ *  suma = a + b;
+ *  return suma
+ * }
+ * 
+ * Es un híbrido entre el planteamiento/lógica 
+ * y el código final
+ */
+
+// https://jsdoc.app/about-getting-started
+/**
+ * Función que calcula la suma
+ * @param {number} a 
+ * @param {number} b 
+ * @returns suma de dos números
+ */
+function sumar (a, b){
+  let suma = a + b;
+  return suma;
+}
