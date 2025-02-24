5963e11fc44aa22b568d87acd1eec6c8cf2f7238
César <cesarlpb@gmail.com>
2025-02-24 10:51:45 +0100

    modifico enunciado del ej 01

--- cliente/ejercicios/8.arrays/enunciados/01.js
+++ cliente/ejercicios/8.arrays/enunciados/01.js
@@ -1,14 +1,22 @@
 // Ej 01 
 
 // En este script deberás crear un array para guardar los nombres de los días de 
-// la semana, empezando por 0 para el domingo. Para comprobar el funcionamiento 
+// la semana, empezando por 0 para el domingo. 
+
+// Para comprobar el funcionamiento 
 // pide al usuario un número entre 0 y 6 y devuelve el nombre del día. 
+
 // Se supone que el dato tecleado estará entre 0 y 6
+
 // Si tecleo el número 4 me deberá decir que el día de la semana es jueves
+// Si se sale del rango (0 - 6) el valor sería undefined => devolverá "día no válido"
 
 /******************************************************************************/
 
 // Pasos para resolver
+
+// input: índice del array
+// output: el día de la semana
 
 // 1. Declaramos variables necesarias (inputs)
 
