cdac9b2c5cf3b01926b7cb189dbeada7fcde973b
César <cesarlpb@gmail.com>
2025-02-24 12:53:03 +0100

    añado enunciado del ej 01 de objetos

---
+++ cliente/ejercicios/09.clases-y-objetos/enunciados/01.js
@@ -0,0 +1,43 @@
+// Ej 01
+
+// Para comenzar vamos a crear un objeto sencillo que se usa para guardar 
+// información sobre las calificaciones de un alumno.
+
+//  El curso contiene tres materias: 
+// Ingles, Programación y HTML, 
+// 
+// y el objeto 
+// contendrá el 
+// nombre del alumno y 
+// la calificación en cada una de ellas. 
+// 
+// El script imprimirá el nombre y la media de sus calificaciones
+// Por ejemplo, guardar las calificaciones de un alumno de 
+// 
+// nombre Juan, Inglés: 9, programacion: 8, HTML: 7. Sacará Nota media de Juan 8
+
+// Ej 01 
+
+// En este script deberás crear un array para guardar los nombres de los días de 
+// la semana, empezando por 0 para el domingo. 
+
+// Para comprobar el funcionamiento 
+// pide al usuario un número entre 0 y 6 y devuelve el nombre del día. 
+
+// Se supone que el dato tecleado estará entre 0 y 6
+
+// Si tecleo el número 4 me deberá decir que el día de la semana es jueves
+// Si se sale del rango (0 - 6) el valor sería undefined => devolverá "día no válido"
+
+/******************************************************************************/
+
+// Pasos para resolver
+
+// input: índice del array
+// output: el día de la semana
+
+// 1. Declaramos variables necesarias (inputs)
+
+// 2. Realizamos operaciones y bucles necesarios
+
+// 3. Escribimos el resultado (output)
