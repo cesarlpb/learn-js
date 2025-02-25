18498ae944763d11b76a51351171c9f42d6429b6
César <cesarlpb@gmail.com>
2025-02-25 13:37:13 +0100

    edit

--- cliente/ejercicios/09.clases-y-objetos/enunciados/03.js
+++ cliente/ejercicios/09.clases-y-objetos/enunciados/03.js
@@ -1,18 +1,19 @@
 // Ej 03
 
 // En esta propuesta debes crear una clase de nombre piedra (con class o con 
-// function) con dos propiedades: masa, volumen y un método densidad que calcule este valor (masa/volumen). Luego instancia al menos un objeto de esta clase, dale un valor a su masa y a su volumen y ejecuta el método densidad.
+// function) con dos propiedades: masa, volumen y un método calcularDensidad() 
+// que calcule este valor (masa/volumen). 
 // 
-// Por ejemplo objeto oro, masa de la muestra 194 gr y volumen 10 cc. 
+// Luego instancia al menos un objeto de esta clase, 
+// dale un valor a su masa y a su volumen y ejecuta el método calcularDensidad().
 // 
-// Densidad deberá devolver 19,4.
+// Por ejemplo objeto oro, masa de la muestra 194 g y volumen 10 cc. 
+// 
+// Densidad deberá devolver 19,4 g / cc.
 
 /******************************************************************************/
 
 // Pasos para resolver
-
-// input: índice del array
-// output: el día de la semana
 
 // 1. Declaramos variables necesarias (inputs)
 
