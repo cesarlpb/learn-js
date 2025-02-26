9c87e4f931baad4d09217ddc5bdc4c0cefe56ff5
César <cesarlpb@gmail.com>
2025-02-26 11:57:17 +0100

    añado solución del ej 04

--- cliente/ejercicios/09.clases-y-objetos/soluciones/solucion_04.js
+++ cliente/ejercicios/09.clases-y-objetos/soluciones/solucion_04.js
@@ -24,25 +24,92 @@ class CuentaBancaria {
     _documentoId,       // string -> no puede estar en blanco
     _paisOrigen         // string -> opcional
   ){
-    this.saldo = _saldoInicial;
     // los demás -> ¿cómo validamos las restricciones?
+    
+    if(this.validarSaldo(_saldoInicial)){
+      this.saldo     = _saldoInicial;
+    } else {
+      // this.saldo     = 0;
+      console.error(`❌ El saldo ${_saldoInicial} no es válido.`);
+      return;
+    }
+  
+    if(this.validarStringNoVacio(_nombreTitular)){
+      this.nombre    = _nombreTitular;
+    } else {
+      console.error(`❌ El nombre ${_nombreTitular} no es válido.`);
+      return;
+    }
+    if(this.validarStringNoVacio(_apellidosTitular)){
+      this.apellidos = _apellidosTitular;
+    } else {
+      console.error(`❌ El apellido ${_apellidosTitular} no es válido.`);
+      return;
+    }
+    // mismas reglas que nombre y apellido
+    if(this.validarStringNoVacio(_documentoId)){
+      this.documentoId = _documentoId;
+    } else {
+      console.error(`❌ El documento ${_documentoId} no es válido.`);
+      return;
+    }
+    
+    // opcional asi que no validamos:
+    this.paisOrigen  = _paisOrigen; // normalmente, se usa una lista de países 
+                                    // válidos para validar si es valor correcto
+                                    // Tip: se puede hacer con un array
   }
   ingresar(cantidadIngreso){
     // números positivos o 0 (no se hace nada)
-
+    if(this.validarSaldo(cantidadIngreso)){
+      this.saldo += cantidadIngreso;
+    }
   }
   retirar(cantidadRetiro){
-    // no vale un número negativo 
-    
     // si recibimos 0 no se hace nada
-    
+    if(cantidadRetiro == 0){
+      return;
+    }
+    // no vale un número negativo 
+    let esCantidadRetiroValida = this.validarSaldo(cantidadRetiro) 
+                                 && 
+                                 cantidadRetiro <= this.saldo;
     // no se puede retirar más de lo que haya en saldo de la cuenta
+    if(esCantidadRetiroValida){
+      this.saldo -= cantidadRetiro;
+    }
   }
   // Otros métodos
   imprimirSaldo(){
     // Formateamos información de la cuenta de forma similar a como 
     // se vería en un cajero (simplificado)
-    
+    let fechaHora        = new Date().toLocaleString(); 
+    let extractoBancario = `
+      Fecha y hora:    ${fechaHora}
+      ---------------------------
+      Titular:         ${this.nombre}, ${this.apellidos}
+      País de Origen:  ${this.paisOrigen}
+      Documento:       ${this.documentoId}
+      ---------------------------
+      Saldo en cuenta: ${this.saldo.toFixed(2)} €
+    `;
+    console.log(extractoBancario);
+  }
+  // Funciones auxiliares
+  validarSaldo(saldo){
+    // no vale NaN, Infinity: 
+    let esNaNoInfinity = Number.isNaN(saldo) || !Number.isFinite(saldo);
+    // -> true si es NaN o es Inf
+
+    // debe ser número no negativo (se admite cero):
+    let esPositivo = Number(saldo) >= 0;
+
+    return !esNaNoInfinity && esPositivo;
+  }
+  validarStringNoVacio(str){
+    // no puede estar vacíos: "" o " " no son válidos
+    // usamos trim() para quitar espacios al principio y al final:
+    return str.trim().length >= 3; // nombre corto: Ana, etc.
   }
 }
 
