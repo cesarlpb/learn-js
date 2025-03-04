7976333624e46f8a929256d8552f063303721133
César <cesarlpb@gmail.com>
2025-03-04 10:47:03 +0100

    añado enunciado del ej 02 de DOM

---
+++ cliente/ejercicios/10.DOM/enunciados/02.js
@@ -0,0 +1,23 @@
+/*
+
+Tienes la página web modelo que debe cambiar el color del titular del negro 
+actual a rojo y además debe quedar centrado. El único cambio será en el bloque 
+cambiar y el código de función cambiar()
+
+El bloque cambiar acepta el click, el color del titular cambia a rojo y queda 
+centrado
+
+*/
+
+/******************************************************************************/
+
+// Pasos para resolver
+
+// input: índice del array
+// output: el día de la semana
+
+// 1. Declaramos variables necesarias (inputs)
+
+// 2. Realizamos operaciones y bucles necesarios
+
+// 3. Escribimos el resultado (output)
