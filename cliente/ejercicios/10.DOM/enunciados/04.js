51ca43fc64ec38305df979cd53e710df334109ad
César <cesarlpb@gmail.com>
2025-03-04 13:03:37 +0100

    actualizo enunciados de ejs 04 y 05

---
+++ cliente/ejercicios/10.DOM/enunciados/04.js
@@ -0,0 +1,23 @@
+/*
+
+Escribe un script de nombre creaDiv() que crea un nuevo bloque div en la página 
+modelo. 
+
+El bloque se creará al pulsar el botón con texto Crear, sus colores serán texto 
+blanco con fondo rojo y el texto quedará centrado. 
+
+Este texto consiste en la cadena: "Javascript permite crear páginas dinámicas".
+El botón ya existe solo es necesario escribir la función, procurando usar los 
+métodos de document para crear nodos.
+
+*/
+
+/******************************************************************************/
+
+// Pasos para resolver
+
+// 1. Declaramos variables necesarias (inputs)
+
+// 2. Realizamos operaciones y bucles necesarios
+
+// 3. Escribimos el resultado (output)
---
+++ cliente/ejercicios/10.DOM/enunciados/05.js
@@ -0,0 +1,22 @@
+/*
+
+La página web modelo contiene tres imágenes. Cada imagen está en un bloque figure 
+con un elemento figcaption para un pie de foto. Al pulsar sobre cualquier imagen 
+se debe mostar su atributo alt en su pie de foto. Si el pie ya está relleno se 
+borrará su contenido y no se escribirá nada.
+
+Cada imagen es como un botón que alterna el pie de foto entre vacio (sin texto) 
+y el valor del atributo alt de cada una.
+
+
+*/
+
+/******************************************************************************/
+
+// Pasos para resolver
+
+// 1. Declaramos variables necesarias (inputs)
+
+// 2. Realizamos operaciones y bucles necesarios
+
+// 3. Escribimos el resultado (output)
