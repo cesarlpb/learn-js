97d984a04f877c041034268ea97043a38a7eaeca
César <cesarlpb@gmail.com>
2025-03-04 10:33:47 +0100

    enunciado 01 de ejercicios de DOM

---
+++ cliente/ejercicios/10.DOM/enunciados/01.js
@@ -0,0 +1,23 @@
+/*
+En la página web modelo ves que tienes un titular Hola Mundo. 
+
+Se trata de añadir una función javascript para que al pulsar el botón cambiar 
+este título se convierta en Hola Mundo con Javascript . El unico cambio 
+necesario es el bloque con el texto cambiar y  completar la función cambiar() 
+que aparece en el código de la página.
+
+Debe cambiar de Hola Mundo a Hola Mundo Javascript
+*/
+
+/******************************************************************************/
+
+// Pasos para resolver
+
+// input: índice del array
+// output: el día de la semana
+
+// 1. Declaramos variables necesarias (inputs)
+
+// 2. Realizamos operaciones y bucles necesarios
+
+// 3. Escribimos el resultado (output)
