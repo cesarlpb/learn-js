72e4881566024dd974f6cacabeb788b9a08c2ae8
César <cesarlpb@gmail.com>
2025-03-05 10:38:39 +0100

    enunciado del ej 06 de mostrar fuentes de imgs

--- cliente/ejercicios/10.DOM/enunciados/06.js
+++ cliente/ejercicios/10.DOM/enunciados/06.js
@@ -1,7 +1,13 @@
 /*
 
-En este script debes leer todos los elementos tipo imagen de la página y mostrar sus urls en un bloque tipo div cuyo id es fuentes y que ya existe en la página modelo usada para estos ejercicios. El script se ejecutará al pulsar el botón con el texto fuentes
-El bloque ya está creado solo tienes que crear la función denominada fuentesImg() asociada al botón fuentes
+En este script debes leer todos los elementos tipo imagen de la página y mostrar 
+sus urls en un bloque tipo div cuyo id es fuentes y que ya existe en la página 
+modelo usada para estos ejercicios. 
+
+El script se ejecutará al pulsar el botón con el texto fuentes.
+
+El bloque ya está creado solo tienes que crear la función denominada 
+fuentesImg() asociada al botón fuentes
 
 */
 
