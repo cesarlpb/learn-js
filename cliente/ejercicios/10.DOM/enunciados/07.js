d0107eec91a31f44662af120e4eb11eae3d8b638
César <cesarlpb@gmail.com>
2025-03-05 10:46:34 +0100

    enunciado del ej 07

--- cliente/ejercicios/10.DOM/enunciados/07.js
+++ cliente/ejercicios/10.DOM/enunciados/07.js
@@ -1,7 +1,11 @@
 /*
 
-El script que tienes que crear en la página modelo debe cambiar la clase del elemento que contiene al encabezado de la página. Al pulsar sobre el titular se añadirá o se eliminarás (toggle) la clase de nombre activo
-Tienes que modificar el botón clase y completar la función toggleClase() para producir este efecto.
+El script que tienes que crear en la página modelo debe cambiar la clase del 
+elemento que contiene al encabezado de la página. Al pulsar sobre el titular se 
+añadirá o se eliminarás (toggle) la clase de nombre activo
+
+Tienes que modificar el botón clase y completar la función toggleClase() para 
+producir este efecto.
 
 */
 
