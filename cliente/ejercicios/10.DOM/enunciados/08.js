0548e51da9ed45eeccb61f49477a2504ebcee750
César <cesarlpb@gmail.com>
2025-03-05 11:57:30 +0100

    enunciado ej 08: intercalar elemento

--- cliente/ejercicios/10.DOM/enunciados/08.js
+++ cliente/ejercicios/10.DOM/enunciados/08.js
@@ -1,6 +1,12 @@
 /*
 
-Escribe el código de una función nuevoBloque() que deberá crear un nuevo elemento div, con una class de nombre destacar. El bloque contendrá, en negrita, el texto Página Modelo. Este bloque debe aparecer justo antes del bloque subtitulo es decir justo después del titular de la página. Se activará con el botón superior que contiene el texto Subtitulo
+Escribe el código de una función nuevoBloque() que deberá crear un nuevo 
+elemento div, con una class de nombre "destacar". El bloque contendrá, en 
+negrita, el texto "Página Modelo". Este bloque debe aparecer justo antes del 
+bloque subtitulo es decir justo después del titular de la página. 
+
+Se activará con el botón superior que contiene el texto Subtítulo
+
 La función se llamara intercalar()
 
 */
