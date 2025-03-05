19c7ebf7a51085dceee9eca003333894d01b9226
César <cesarlpb@gmail.com>
2025-03-05 12:45:10 +0100

    enunciado ej 10: toggle de botones

--- cliente/ejercicios/10.DOM/enunciados/10.js
+++ cliente/ejercicios/10.DOM/enunciados/10.js
@@ -1,7 +1,13 @@
 /*
 
-El primer botón de la barra de botones en la parte superior de la página tiene el texto abrir/cerrar. Al pulsarlo se deben ocultar o mostrar los botones restantes, pero el de abrir/cerrrar debe quedar visible.
-El valor de la propiedad display de los botones es inline-block. La barra tiene en el atributo class el valor botones y cada botón tiene como clase boton
+El primer botón de la barra de botones en la parte superior de la página tiene 
+el texto abrir/cerrar. Al pulsarlo se deben ocultar o mostrar los botones 
+restantes, pero el de abrir/cerrrar debe quedar visible.
+
+El valor de la propiedad display de los botones es inline-block. 
+
+La barra tiene en el atributo class el valor "botones" y cada botón tiene como 
+clase "boton".
 
 */
 
