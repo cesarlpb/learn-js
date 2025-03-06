86037a05aa8936690de8b73226049de8c867c6bd
César <cesarlpb@gmail.com>
2025-03-06 11:57:43 +0100

    añado solución del ej 03 de calculadora

--- cliente/ejercicios/11.eventos/soluciones/solucion_03.js
+++ cliente/ejercicios/11.eventos/soluciones/solucion_03.js
@@ -1,7 +1,9 @@
 /*
-En esta págian web debes agregar un script para asignar un manejador de eventos 
-al botón button. Este script deberá escribir en el campo total el valor de 
-multiplicar los otros dos campos.
+En esta página web debes agregar un script para asignar un manejador de eventos
+(event listener) al botón button. 
+
+Este script deberá escribir en el campo total el valor de multiplicar los otros 
+dos campos.
 
 Formulario en 03.html
 
@@ -14,6 +16,18 @@ de calcular  aparecerá 40 en el campo total. Coloca el script al final del body
 // Pasos para resolver
 
 // 1. Declaramos variables necesarias (inputs)
+const botonEnviar = document.getElementById("enviar");
+botonEnviar.addEventListener("click", function(){
+  // seleccionamos los elementos input y nos quedamos con sus valores como Number:
+  const precio = document.getElementById("precio").valueAsNumber;
+  const cantidad = document.getElementById("cantidad").valueAsNumber;
+  // calculamos el importe total:
+  const importeTotal = precio * cantidad;
+  // seleccionamos el elmento donde colocamos el resultado y lo actualizamos ahí:
+  const importeTotalEl = document.getElementById("total");
+  importeTotalEl.value = importeTotal.toFixed(2) + " €";
+  // Se puede añadir descuentos, IVA...
+})
 
 // 2. Realizamos operaciones y bucles necesarios
 
