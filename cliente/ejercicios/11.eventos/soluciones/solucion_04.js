48a219e4e85a1ab6137d35d8e7eafd30bab45e6e
César <cesarlpb@gmail.com>
2025-03-06 12:25:40 +0100

    añado solución del ej 04 de eventos de carga de página y del DOM

--- cliente/ejercicios/11.eventos/soluciones/solucion_04.js
+++ cliente/ejercicios/11.eventos/soluciones/solucion_04.js
@@ -1,7 +1,9 @@
 /*
 Crea un script para que se active cuando la página se haya cargado completamente 
-(evento load de window). Una vez cargada la página el script asignará un evento 
-al bloque de manera que al entrar el ratón su fondo pase a rojo y al salir vuelva al blanco.
+(evento load de window). 
+
+Una vez cargada la página el script asignará un evento al bloque de manera que 
+al entrar el ratón su fondo pase a rojo y al salir vuelva al blanco.
 
 El script se colocará en la sección <head> de la página.
 
@@ -13,8 +15,33 @@ cargado, por eso el  evento onload.
 
 /******************************************************************************/
 
-// Pasos para resolver
+let horaUnix1, horaUnix2;
+let div;
 
+// Pasos para resolver
+window.addEventListener("load", function(){
+  horaUnix1 = new Date().getTime();
+  console.log("La página ha cargado!", "Hora Unix:", horaUnix1)
+  // actualizamos aquí la diferencia porque el evento load sucede después del de 
+  // DOM:
+  console.log(`Hay una diferencia de: ${Math.abs(horaUnix1 - horaUnix2)} ms`);
+  /*****************************************************************************/
+  // Ej 04 aquí:
+  div = document.getElementById("cuadro");
+  div.addEventListener("mouseenter", cambiarBackgroundRojo);
+  div.addEventListener("mouseleave", cambiarBackgroundBlanco);
+});
+window.addEventListener("DOMContentLoaded", function(){
+  horaUnix2 = new Date().getTime();
+  console.log("El DOM ha cargado!", "Hora Unix:", horaUnix2)
+});
+// También se pueden usar dos funciones anónimas en vez de definir estas dos:
+function cambiarBackgroundRojo(){
+  div.style.backgroundColor = "red";
+}
+function cambiarBackgroundBlanco(){
+  div.style.backgroundColor = "white";
+}
 // 1. Declaramos variables necesarias (inputs)
 
 // 2. Realizamos operaciones y bucles necesarios
