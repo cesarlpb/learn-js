69cd2572ab157f4f2ce6c7665c1181920c9a4e4d
César <cesarlpb@gmail.com>
2025-03-06 10:49:17 +0100

    añado solución del ej 01 de eventos

--- cliente/ejercicios/11.eventos/soluciones/solucion_01.js
+++ cliente/ejercicios/11.eventos/soluciones/solucion_01.js
@@ -1,10 +1,16 @@
 /*
-En una página web vas a colocar un elemento tipo div con un argumento onclick() 
+En una página web vas a colocar un elemento tipo div con un atributo onclick()
+
+-> vamos a colocar el event listener desde JS
+
 para que al pulsar sobre el bloque se abra una ventana alert con el mensaje 
-"Pulsaste sobre el botón". El bloque div deberá tener como texto "Pulsa aquí".
-Al pulsar sobre el ratón sobre el bloque saltará la ventana alert. El bloque div 
-puede ser un bloque simple, o puedes aplicarle estilos css para que quede como 
-un botón.
+"Pulsaste sobre el botón". 
+
+El bloque div deberá tener como texto "Pulsa aquí".
+
+Al pulsar el ratón sobre el bloque saltará la ventana alert. 
+El bloque div puede ser un bloque simple, o puedes aplicarle estilos CSS para 
+que quede como un botón.
 */
 
 /******************************************************************************/
@@ -12,6 +18,15 @@ un botón.
 // Pasos para resolver
 
 // 1. Declaramos variables necesarias (inputs)
+
+// seleccionamos el botón por id:
+const boton = document.getElementById("mi-boton");
+// añadimos el listener (oyente) de eventos:
+boton.addEventListener("click", function() {
+  alert("Pulsaste sobre el botón");
+});
+// -> function(){} sin nombre es fn anónima y se usa para definir fn donde 
+// se necesita
 
 // 2. Realizamos operaciones y bucles necesarios
 
