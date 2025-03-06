282490fef52c12ae0f541aebc4d0cada81470000
César <cesarlpb@gmail.com>
2025-03-06 12:38:02 +0100

    añado solución del ej 05 de sacar id de event.target

--- cliente/ejercicios/11.eventos/soluciones/solucion_05.js
+++ cliente/ejercicios/11.eventos/soluciones/solucion_05.js
@@ -1,9 +1,13 @@
 /*
+
 En esta página tienes dos bloques div a modo de botones, como ves en este código 
-HTML de abajo. Debes escribir una única función Javascript (llamada pulsar) para 
-manejar los eventos click de ambos botones. Cuando pulses con el ratón en 
-cualquier botón deberá aparecer una ventana alert indicando el id del botón 
-donde se ha pulsado.
+HTML de abajo. 
+
+Debes escribir una única función Javascript (llamada pulsar() ) para 
+manejar los eventos click de ambos botones. 
+
+Cuando pulses con el ratón en cualquier botón deberá aparecer una ventana alert 
+indicando el id del botón donde se ha pulsado.
 
 05.html
 
@@ -17,6 +21,18 @@ página.
 
 // 1. Declaramos variables necesarias (inputs)
 
+// Seleccionamos los elementos:
+const boton1 = document.getElementById("boton1");
+const boton2 = document.getElementById("boton2");
+// Colocamos los listeners de click:
+boton1.addEventListener("click", pulsar);
+boton2.addEventListener("click", pulsar);
+// Definimos la fn pulsar que recibe el parámetro "comodín" event que tiene
+// información del evento que se ha registrado
+function pulsar(event) {
+  alert(event.target.id); // dentro de target hay información del elemento del 
+                          // DOM que genera el evento
+}
 // 2. Realizamos operaciones y bucles necesarios
 
 // 3. Escribimos el resultado (output)
