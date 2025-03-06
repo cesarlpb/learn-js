0143cfcd5afaedbc0951cd13497137bf82e2e705
César <cesarlpb@gmail.com>
2025-03-06 11:37:53 +0100

    añado solución del ej 02 de listener de ratón

--- cliente/ejercicios/11.eventos/soluciones/solucion_02.js
+++ cliente/ejercicios/11.eventos/soluciones/solucion_02.js
@@ -1,11 +1,18 @@
 /*
-Necesitas una página web con dos bloques div. El primer bloque contiene el texto 
-"Pasa por aqui" y el segundo "Efectos del movimiento" con un argumento 
-id = "efecto" . Cuando el ratón pase por el primer bloque se llamará a la 
-función color que cambia el fondo del segundo bloque a color verde 
+
+Necesitas una página web con dos bloques div:
+
+El primer bloque contiene el texto  "Pasa por aqui" y el segundo  tiene: 
+"Efectos del movimiento" 
+
+con un atributo id="efecto". 
+
+Cuando el ratón pase por el primer bloque se llamará a la función color() que 
+cambia el fondo del segundo bloque a color verde 
 (backgroundColo:  green")
-Recuerda que los nodos se pueden seleccionar con getElementById(). 
-Usa estilos en linea (style)
+
+- Recuerda que los nodos se pueden seleccionar con getElementById(). 
+- Usa estilos en linea (style)
 */
 
 /******************************************************************************/
@@ -13,6 +20,30 @@ Usa estilos en linea (style)
 // Pasos para resolver
 
 // 1. Declaramos variables necesarias (inputs)
+const div1 = document.getElementById("boton-1");
+const div2 = document.getElementById("efecto");
+let estaEnVerde = false;
+
+function color(){
+  div2.style.backgroundColor = "green";
+  estaEnVerde = true;
+}
+function cambiarColor(event) {
+  if(!estaEnVerde){
+    // console.log(event) // objeto MouseEvent con información del evento
+    color();
+  }
+}
+
+// Nota: nos pueden valer estos eventos de ratón:
+// mouseenter := cuando el cursor entra en el elemento
+// mouseover  := cuando el cursor está encima del elemento
+// mousemove  := cuando el cursor se desplaza en el elemento => hace muchas llamadas
+
+div1.addEventListener("mouseenter", cambiarColor, false);
+
+// div1.removeEventListener("mouseenter", cambiarColor, false);
+// también se puede borrar el listener una vez se haya utilizado y ya no se necesite
 
 // 2. Realizamos operaciones y bucles necesarios
 
