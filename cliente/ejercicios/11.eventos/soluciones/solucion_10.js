e8f53cd7da31aabaf7cece3ae4ebb2debd4699c0
César <cesarlpb@gmail.com>
2025-03-07 10:50:32 +0100

    añado solución del ej 10 de evento input

--- cliente/ejercicios/11.eventos/soluciones/solucion_10.js
+++ cliente/ejercicios/11.eventos/soluciones/solucion_10.js
@@ -14,6 +14,24 @@ input para no admitir la tecla pulsada.
 // Pasos para resolver
 
 // 1. Declaramos variables necesarias (inputs)
+let prev = ""
+const input = document.addEventListener("input", function(event){
+  let valorActual = event.target.value;
+  if(!prev){
+    prev = valorActual;
+  } 
+  
+  let nuevoChar = valorActual[valorActual.length - 1];
+  if(!Number.isNaN(Number(nuevoChar))){
+    event.target.value = prev;
+  }
+
+  console.log(prev, valorActual)
+  prev = event.target.value;
+});
+
+// ¿Cómo se puede extender para una lista de caracteres que no permitimos en el 
+// input? ( Usando un array de caracteres no permitidos )
 
 // 2. Realizamos operaciones y bucles necesarios
 
